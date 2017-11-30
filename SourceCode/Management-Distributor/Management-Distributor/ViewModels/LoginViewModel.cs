2016cbdec80f74cb6036034520dd74ccb10c70be
1412183 <32013225+1412183@users.noreply.github.com>
2017-11-30 18:39:01 +0700

    add login view model

---
+++ SourceCode/Management-Distributor/Management-Distributor/ViewModels/LoginViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Management_Distributor.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Please provide username or email")]
+        public string usernameOremail { get; set; }
+        [Required(ErrorMessage = "Please provide password associated with your username")]
+        public string passwordRaw { get; set; }
+    }
+}
