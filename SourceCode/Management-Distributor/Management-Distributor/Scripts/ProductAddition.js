9cbf8cbce7cfb401e8b4c6f911be32b1156ac947
1412183 <32013225+1412183@users.noreply.github.com>
2017-12-15 21:42:58 +0700

    init

---
+++ SourceCode/Management-Distributor/Management-Distributor/Scripts/ProductAddition.js
@@ -0,0 +1,34 @@
+﻿
+$(function () {
+    $("#loaderbody").addClass('hide');
+
+
+    $(document).bind('ajaxStart', function () {
+        $("#loaderbody").removeClass('hide');
+    }).bind('ajaxStop', function () {
+        $("#loaderbody").addClass('hide');
+    });
+});
+
+$(document).ready(function() {
+    $('#loaderbody').addClass('hide');
+    $(document).ajaxStart(function () {
+        $('#loaderbody').removeClass('hide');
+    }).ajaxStop(function () {
+        $('#loaderbody').addClass('hide');
+    });
+});
+
+
+
+
+function ShowImagePreview(imageUploader, previewImage) {
+    if (imageUploader.files && imageUploader.files[0]) {
+        var reader = new FileReader();
+        reader.onload = function (e) {
+            $(previewImage).attr('src', e.target.result);
+        }
+        reader.readAsDataURL(imageUploader.files[0]);
+    }
+}
+
