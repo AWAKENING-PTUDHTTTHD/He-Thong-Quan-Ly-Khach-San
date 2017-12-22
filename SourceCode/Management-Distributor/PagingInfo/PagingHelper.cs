3b1b2d035790a487cbda8a2449daa5ee31357427
1412183 <32013225+1412183@users.noreply.github.com>
2017-12-22 14:36:32 +0700

    update

--- SourceCode/Management-Distributor/PagingInfo/PagingHelper.cs
+++ SourceCode/Management-Distributor/PagingInfo/PagingHelper.cs
@@ -10,26 +10,25 @@ namespace Pagination
     public static class PagingHelper
     {
 
-        public static MvcHtmlString PageLink(PagingInfo pagingInfo, Func<int, string> pageUrl)
+        public static MvcHtmlString PageLinks(this HtmlHelper html,
+                                                PagingInfo pagingInfo,
+                                                Func<int, string> pageUrl)
         {
-
             var result = new StringBuilder();
-            for (int i = 1; i <= pagingInfo.TotalPages;i++)
+            for (int i = 1; i <= pagingInfo.TotalPages; i++)
             {
                 var tag = new TagBuilder("a");
                 tag.MergeAttribute("href", pageUrl(i));
                 tag.InnerHtml = i.ToString();
-                if(i == pagingInfo.CurrentPage)
+                if (i == pagingInfo.CurrentPage)
                 {
                     tag.AddCssClass("selected");
-                    tag.AddCssClass("btn-warning");
+                    tag.AddCssClass("btn-primary");
                 }
                 tag.AddCssClass("btn btn-default");
                 result.Append(tag.ToString());
             }
-
-            return MvcHtmlString.Create(result.ToString());
-
+            return System.Web.Mvc.MvcHtmlString.Create(result.ToString());
         }
 
     }
