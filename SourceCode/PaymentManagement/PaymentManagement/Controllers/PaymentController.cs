70c99e3cc9c83f787ce37a7cc6bac2163b9705d9
1312404 <quynhnhi220895@gmail.com>
2017-12-22 23:38:30 +0700

    Payment management (Error)

---
+++ SourceCode/PaymentManagement/PaymentManagement/Controllers/PaymentController.cs
@@ -0,0 +1,111 @@
+﻿using PaymentManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PaymentManagement.Controllers
+{
+    public class PaymentController : Controller
+    {
+      
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult getPayment()
+        {
+            using (MyDatabaseEntities dc = new MyDatabaseEntities())
+            {
+                var pay = dc.Payments.OrderBy(a => a.InvoiceID).ToList();
+                return Json(new { data = pay }, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+
+        ///// Save
+        [HttpGet]
+        public ActionResult save(int id)
+        {
+            using (MyDatabaseEntities dc = new MyDatabaseEntities())
+            {
+                var v = dc.Payments.Where(a => a.PaymentID == id).FirstOrDefault();
+                return View(v);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult save(Payment pay)
+        {
+            bool status = false;
+            if (ModelState.IsValid)
+            {
+                using (MyDatabaseEntities dc = new MyDatabaseEntities())
+                {
+                    if (pay.PaymentID > 0)
+                    {
+                        //Edit 
+                        var v = dc.Payments.Where(a => a.PaymentID == pay.PaymentID).FirstOrDefault();
+                        if (v != null)
+                        {
+
+                            v.InvoiceID = pay.InvoiceID;
+                            v.Amount = pay.Amount;
+                            v.PaidDate = pay.PaidDate;
+                            v.ExceedDate = pay.ExceedDate;
+                            v.Status = pay.Status;
+                            v.Note = pay.Note;
+
+                        }
+                    }
+                    else
+                    {
+                        //Save
+                        dc.Payments.Add(pay);
+                    }
+                    dc.SaveChanges();
+                    status = true;
+                }
+            }
+            return new JsonResult { Data = new { status = status } };
+        }
+
+        ///// Delete
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            using (MyDatabaseEntities dc = new MyDatabaseEntities())
+            {
+                var v = dc.Payments.Where(a => a.PaymentID == id).FirstOrDefault();
+                if (v != null)
+                {
+                    return View(v);
+                }
+                else
+                {
+                    return HttpNotFound();
+                }
+            }
+        }
+
+        [HttpPost]
+        [ActionName("Delete")]
+        public ActionResult DeletePayment(int id)
+        {
+            bool status = false;
+            using (MyDatabaseEntities dc = new MyDatabaseEntities())
+            {
+                var v = dc.Payments.Where(a => a.PaymentID == id).FirstOrDefault();
+                if (v != null)
+                {
+                    dc.Payments.Remove(v);
+                    dc.SaveChanges();
+                    status = true;
+                }
+            }
+            return new JsonResult { Data = new { status = status } };
+        }  
+	}
+}
---
+++ SourceCode/PaymentManagement/PaymentManagement/Scripts/myScripts.js
@@ -0,0 +1,109 @@
+﻿/////// getInvoiceList
+var invoices = []
+//fetch categories from database
+function loadInvoice(element) {
+    if (invoices.length == 0) {
+        //ajax function for fetch data
+        $.ajax({
+            type: "GET",
+            url: '/payment/getInvoice',
+            success: function (data) {
+                invoices = data;
+                //render catagory
+                renderInvoice(element);
+            }
+        })
+    }
+    else {
+        //render catagory to the element
+        renderInvoice(element);
+    }
+}
+
+function renderInvoice(element) {
+    var $ele = $(element);
+    $ele.empty();
+    $ele.append($('<option/>').val('0').text('Select'));
+    $.each(invoices, function (i, val) {
+        $ele.append($('<option/>').val(val.InvoiceID).text(val.InvoiceName));
+    })
+}
+
+
+/////// Require 
+$(document).ready(function () {
+
+    var oTable = $('#myDatatable').DataTable({
+        "ajax": {
+            "url": '/payment/getPayment',
+            "type": "get",
+            "datatype": "json"
+        },
+        "columns": [
+            { "data": "InvoiceID", "autoWidth": true },
+            { "data": "Amount", "autoWidth": true },
+            { "data": "PaidDate", "autoWidth": true },
+            { "data": "ExceedDate", "autoWidth": true },
+            { "data": "Status", "autoWidth": true },
+            { "data": "Note", "autoWidth": true },
+            {
+                "data": "PaymentID", "width": "50px", "render": function (data) {
+                    return '<a class="popup" href="/payment/save/' + data + '">Edit</a>';
+                }
+            },
+            {
+                "data": "PaymentID", "width": "50px", "render": function (data) {
+                    return '<a class="popup" href="/payment/delete/' + data + '">Delete</a>';
+                }
+            }
+        ]
+    })
+    $('.tablecontainer').on('click', 'a.popup', function (e) {
+        e.preventDefault();
+        OpenPopup($(this).attr('href'));
+    })
+
+
+    function OpenPopup(pageUrl) {
+        var $pageContent = $('<div/>');
+        $pageContent.load(pageUrl, function () {
+            $('#popupForm', $pageContent).removeData('validator');
+            $('#popupForm', $pageContent).removeData('unobtrusiveValidation');
+            $.validator.unobtrusive.parse('form');
+
+        });
+
+        $dialog = $('<div class="popupWindow" style="overflow:auto"></div>')
+                  .html($pageContent)
+                  .dialog({
+                      draggable: false,
+                      autoOpen: false,
+                      resizable: false,
+                      model: true,
+                      title: 'Popup Dialog',
+                      height: 550,
+                      width: 600,
+                      close: function () {
+                          $dialog.dialog('destroy').remove();
+                      }
+                  })
+
+        $('.popupWindow').on('submit', '#popupForm', function (e) {
+            var url = $('#popupForm')[0].action;
+            $.ajax({
+                type: "POST",
+                url: url,
+                data: $('#popupForm').serialize(),
+                success: function (data) {
+                    if (data.status) {
+                        $dialog.dialog('close');
+                        oTable.ajax.reload();
+                    }
+                }
+            })
+
+            e.preventDefault();
+        })
+        $dialog.dialog('open');
+    }
+})
